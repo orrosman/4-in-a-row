cf4803e3c6ca748469e65cc8964a9c5312a6924e
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-10-28 20:30:20 +0300

    Added function to search for victory horizontly

--- Model.js
+++ Model.js
@@ -16,9 +16,48 @@ class Connect4 {
 		for (let i = 0; i < this._board[colum].length; i++) {
 			if (!this._board[colum][i]) {
 				this._board[colum].splice(i, 1, player);
+				this.switchPlayer();
 				return;
 			}
 		}
+	}
+
+	switchPlayer() {
+		if (this._currentPlayer == 'Blue') {
+			this._currentPlayer = 'Red';
+		} else {
+			this._currentPlayer = 'Blue';
+		}
+	}
+
+	//gets a start point
+	checkWinHorizontal(colum, row, color) {
+		let count = 0;
+		for (let i = 0; i < 4; i++) {
+			if (this._board[colum + i][row] == color && colum + i < 7) {
+				count++;
+				if (count == 4) {
+					return true;
+				}
+			} else {
+				count = 0;
+				break;
+			}
+		}
+		count = 0;
+		for (let i = 0; i < 4; i++) {
+			if (this._board[colum - i][row] == color && colum - i > 0) {
+				count++;
+				if (count == 4) {
+					return true;
+				}
+			} else {
+				count = 0;
+				break;
+			}
+		}
+
+		return false;
 	}
 
 	play(move) {
